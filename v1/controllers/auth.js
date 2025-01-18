ed1d162bbe7dd62584ef7bdbad235222ec2d9504
Kevin Jiménez Garro <mkjg16@gmail.com>
2025-01-18 08:42:09 -0600

    First commit

---
+++ v1/controllers/auth.js
@@ -0,0 +1,96 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+/**
+ * @route POST v1/auth/register
+ * @desc Registers a user
+ * @access Public
+ */
+export async function Register(req, res) {
+    // get required variables from request body
+    // using es6 object destructing
+    const { first_name, last_name, email, password } = req.body;
+
+    // const first_name = req.body.first_name;
+    // const  last_name = req.body.last_name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    try {
+        // create an instance of a user
+        const newUser = new User({
+            first_name,
+            last_name,
+            email,
+            password,
+        });
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(400).json({
+                status: "failed",
+                data: [],
+                message: "It seems you already have an account, please log in instead.",
+            });
+        const savedUser = await newUser.save(); // save new user into the database
+        const { role, ...user_data } = savedUser._doc;
+        res.status(200).json({
+            status: "success",
+            data: [user_data],
+            message:
+                "Thank you for registering with us. Your account has been successfully created.",
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Internal Server Error",
+        });
+    }
+    res.end();
+}
+
+export async function Login(req, res) {
+    // Get variables for the login process
+    const { email } = req.body;
+    try {
+        // Check if user exists
+        const user = await User.findOne({ email }).select("+password");
+        if (!user)
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message:
+                    "Invalid email or password. Please try again with the correct credentials.",
+            });
+        // if user exists
+        // validate password
+        const isPasswordValid = await bcrypt.compare(
+            `${req.body.password}`,
+            user.password
+        );
+        // if not valid, return unathorized response
+        if (!isPasswordValid)
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message:
+                    "Invalid email or password. Please try again with the correct credentials.",
+            });
+        // return user info except password
+        const { password, ...user_data } = user._doc;
+
+        res.status(200).json({
+            status: "success",
+            data: [user_data],
+            message: "You have successfully logged in.",
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Internal Server Error",
+        });
+    }
+    res.end();
+}
---
+++ v1/routes/index.js
@@ -0,0 +1,24 @@
+import Auth from "./auth.js";
+
+const Router = (server) => {
+    // home route with the get method and a handler
+    server.get("/v1", (req, res) => {
+        try {
+            res.status(200).json({
+                status: "success",
+                data: [],
+                message: "Welcome to our API homepage!",
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: "error",
+                message: "Internal Server Error",
+            });
+        }
+    })
+
+    server.use("/v1/auth", Auth);
+
+
+};
+export default Router;
